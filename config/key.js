81180422cab59d2bea53a53a027e5d539933d99d
Pranit <pranitarora074@gmail.com>
2019-02-25 21:18:44 +0530

    adding modules

---
+++ config/key.js
@@ -0,0 +1,13 @@
+module.exports = {
+mongoURI:'mongodb+srv://Pranit:Pranit1998@cluster0-yttwx.mongodb.net/test?retryWrites=true',
+google:{
+    clientID:'428281306547-13vgpngg7q6tnpv3d54pb9pp31fepb1o.apps.googleusercontent.com',
+    clientsecret:'ZX5sbWzEpwQohjojT1Chyels'
+},
+
+mail:{
+    user: 'eduassetsinfo@gmail.com',
+    pass: 'Edu@1998'
+}
+
+}      
